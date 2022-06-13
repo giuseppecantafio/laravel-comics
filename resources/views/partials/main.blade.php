9a515897ebc3256e387c06c72fc1047becc99457
Giuseppe <giuseppecantafio@hotmail.it>
2022-06-13 19:47:54 +0200

    aggiunte cards

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,3 +1,16 @@
-<main>
-    sono il main
+ <main>
+      <div class="main-wrapper">
+         @foreach ($datiMain as $elm)
+            <div class="boxes"">
+                <div class="img-wrapper">
+                <img src="{{$elm['thumb']}}" alt="{{$elm['series']}}" />
+                </div>
+                <div class="text-container">
+                    <span>{{$elm['series']}}</span>
+                </div>
+            </div>
+            @endforeach
+            <div class="button-container">
+                <span class="button">Load more</span>
+            </div>
 </main>
